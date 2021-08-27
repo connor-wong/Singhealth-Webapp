619a3b4d3aeaa0b80bfa1b1f040bbde6160de8aa
Connor Wong <s10197876@connect.np.edu.sg>
2021-08-27 15:06:14 +0800

    Removed FireBase API Keys Details

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -6,13 +6,13 @@ import "firebase/storage";
 const settings = { timestampsInSnapshots: true };
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyDp75dGgj-VJul28kUM3aNYtKYdrm28oaQ",
-  authDomain: "check-list-development.firebaseapp.com",
-  databaseURL: "https://check-list-development-default-rtdb.firebaseio.com",
-  projectId: "check-list-development",
-  storageBucket: "check-list-development.appspot.com",
-  messagingSenderId: "683689026143",
-  appId: "1:683689026143:web:e7d5d846483a5f75d271ea",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 });
 
 firebase.firestore().settings(settings);
